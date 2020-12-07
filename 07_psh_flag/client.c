edbc3c4dec42b854670798e539d5a0369e5cd184
Raphael Mader <mader.raphael@gmx.at>
2020-12-07 19:49:58 +0100

    Add test for push flag

---
+++ 07_psh_flag/client.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "..\\socket.h"
+
+// Just a bit more than MSS
+#define BUFFER_SIZE 1461
+static char buffer[BUFFER_SIZE];
+
+static const char *const serverIp = "192.168.1.1";
+static const u_short serverPort = 9000;
+
+void sendMessageToServer(SOCKET socket, const char *message, size_t size);
+void recieveMessageFromServer(SOCKET socket);
+
+int main()
+{
+    initializeWinsock();
+
+    const SOCKET clientSocket = createSocket();
+
+    connectToServerSocket(clientSocket, serverIp, serverPort);
+    unblock(clientSocket);
+
+    memset(buffer, 'X', BUFFER_SIZE);
+    sendAllData(clientSocket, buffer, BUFFER_SIZE);
+
+    // Graceful shutdown
+    shutdownSocket(clientSocket, SD_BOTH);
+    closeSocket(clientSocket);
+
+    cleanupWinsock();
+
+    return EXIT_SUCCESS;
+}
+
+void sendMessageToServer(const SOCKET socket, const char *const message, const size_t size)
+{
+    sendAllData(socket, message, size);
+}
+
+void recieveMessageFromServer(const SOCKET socket)
+{
+    receiveUntil(socket, buffer, BUFFER_SIZE, 'Y');
+}
